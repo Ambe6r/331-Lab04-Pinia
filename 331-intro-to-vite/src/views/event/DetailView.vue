63bcb32fdf7b593d3c9f25e08e0a1b561cca075e
Amber <1448990064@qq.com>
2024-08-06 21:40:41 +0700

    1.6

--- 331-intro-to-vite/src/views/event/DetailView.vue
+++ 331-intro-to-vite/src/views/event/DetailView.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
 import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+import { onUnmounted } from 'vue'
 
 const props = defineProps<{
   event: Event
   id: String
 }>()
 const { event } = toRefs(props)
+const messageStore = useMessageStore()
+const { message } = storeToRefs(messageStore)
 
+onUnmounted(() => {
+  messageStore.resetMessage()
+})
 </script>
 
 <template>
-  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-  <p>{{ event.description }}</p>
+  <div>
+    <div id="flashMessage" v-if="message">
+      <h4>{{ message }}</h4>
+    </div>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
 </template>
+
+<style scoped>
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
+}
+</style>
---
+++ 331-intro-to-vite/src/views/event/EditView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+import { type Event } from '@/types'
+import { useMessageStore } from '@/stores/message'
+
+const props = defineProps<{
+  event: Event
+  id: string
+}>()
+const { event } = toRefs(props)
+const router = useRouter()
+const messageStore = useMessageStore()
+
+const goToDetails = () => {
+  messageStore.updateMessage('Data has been updated')
+  router.push({ name: 'event-detail-view', params: { id: props.id } })
+}
+</script>
+
+<template>
+  <p>Edit event here</p>
+  <button @click="goToDetails">Edit</button>
+</template>
