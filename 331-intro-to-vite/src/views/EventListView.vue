8a1daa39eb2ea499729b95cefa8e6ae34e8535ea
Amber <1448990064@qq.com>
2024-08-06 21:52:53 +0700

    2.3

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -4,7 +4,6 @@ import CategoryCard from '@/components/CategoryCard.vue'
 import { type Event } from '@/types'
 import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
-import nProgress from 'nprogress'
 
 const events = ref<Event[] | null>(null)
 const totalEvents = ref(0)
@@ -28,7 +27,6 @@ const perPage = computed(() => props.perPage)
 onMounted(() => {
   watchEffect(() => {
     events.value = null
-    nProgress.start()
     EventService.getEvents(perPage.value, page.value)
       .then((response) => {
         events.value = response.data
@@ -36,9 +34,6 @@ onMounted(() => {
       })
       .catch((error) => {
         console.error('There was an error!', error)
-      })
-      .finally(() => {
-        nProgress.done()
       })
   })
 })
